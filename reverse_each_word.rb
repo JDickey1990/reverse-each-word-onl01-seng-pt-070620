d9898e91f4eca7f2a6a40fdac7964ace33e2df01
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-12 21:19:28 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+def reverse_each_word(sentence)
+  backwards=[]
+  sentence.split.each do |word|
+   backwards<< word.reverse
+  end
+   backwards.join(" ")
+end
+
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| 
+    word.reverse}.join(" ")
+end
